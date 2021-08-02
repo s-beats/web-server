2203a1e5a662fba9061b6c3bba77f539577476c6
s-beats <sakashin10291029@gmail.com>
2021-08-02 13:45:47 +0900

    feat: add net package server

---
+++ backend/httpserver/server.go
@@ -0,0 +1,42 @@
+package httpserver
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+// https://golang.org/doc/articles/wiki/#tmp_1
+
+func Start() {
+	middleware := func(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			// CORS の許可
+			w.Header().Set("Access-Control-Allow-Headers", "*")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTION")
+			w.Header().Set("Content-Type", "application/json")
+			f(w, r)
+		}
+	}
+
+	// health check
+	http.HandleFunc("/health", middleware(func(w http.ResponseWriter, r *http.Request) {
+		b, err := json.Marshal("ok")
+		if err != nil {
+			panic(err)
+		}
+		io.WriteString(w, string(b))
+	}))
+
+	// example
+	http.HandleFunc("/ping", middleware(func(w http.ResponseWriter, r *http.Request) {
+		b, err := json.Marshal("{ping:pong}")
+		if err != nil {
+			panic(err)
+		}
+		io.WriteString(w, string(b))
+	}))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
--- backend/netserver/server.go
+++ backend/netserver/server.go
@@ -1,41 +1,22 @@
 package netserver
 
 import (
-	"encoding/json"
-	"io"
 	"log"
-	"net/http"
+	"net"
+
+	"github.com/go-pp/pp"
 )
 
-// https://golang.org/doc/articles/wiki/#tmp_1
-
 func Start() {
-	middleware := func(f http.HandlerFunc) http.HandlerFunc {
-		return func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Headers", "*")
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTION")
-			w.Header().Set("Content-Type", "application/json")
-			f(w, r)
-		}
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rw, err := ln.Accept()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// health check
-	http.HandleFunc("/health", middleware(func(w http.ResponseWriter, r *http.Request) {
-		b, err := json.Marshal("ok")
-		if err != nil {
-			panic(err)
-		}
-		io.WriteString(w, string(b))
-	}))
-
-	// example
-	http.HandleFunc("/ping", middleware(func(w http.ResponseWriter, r *http.Request) {
-		b, err := json.Marshal("{ping:pong}")
-		if err != nil {
-			panic(err)
-		}
-		io.WriteString(w, string(b))
-	}))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// TODO: net.TCPConnを操作してresponseを作る
+	pp.Println(rw)
 }
