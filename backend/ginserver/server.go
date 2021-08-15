399d493744482bca1a8ebdd6654f9bbd79158a0b
s-beats <sakashin10291029@gmail.com>
2021-08-15 10:53:24 +0900

    feat: c.Next()

--- backend/ginserver/server.go
+++ backend/ginserver/server.go
@@ -1,14 +1,35 @@
 package ginserver
 
 import (
+	"log"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+func authorization(c *gin.Context) {
+	// before request
+	log.Println("auth...")
+
+	c.Next()
+
+	// after request
+	log.Println("after handling...")
+}
+
 func Start() {
 	r := gin.Default()
+
+	// CORS middleware
 	r.Use(cors.Default())
-	r.GET("/ping", func(c *gin.Context) {
+
+	// Authorization middleware
+	r.Use(authorization)
+	// /auth/*
+	auth := r.Group("/auth")
+
+	// /auth/ping
+	auth.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
