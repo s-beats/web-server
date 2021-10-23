5108be0f8aa190a2ce38b2fe4534e10a1327836e
s-beats <sakashin10291029@gmail.com>
2021-10-23 15:51:38 +0900

    log middleware

--- backend/ginserver/server.go
+++ backend/ginserver/server.go
@@ -2,9 +2,12 @@ package ginserver
 
 import (
 	"log"
+	"os"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
 )
 
 func authorization(c *gin.Context) {
@@ -12,13 +15,39 @@ func authorization(c *gin.Context) {
 	log.Println("auth...")
 
 	c.Next()
+}
 
-	// after request
-	log.Println("after handling...")
+func logging(c *gin.Context) {
+	start := time.Now() // アクセスを受けた時間
+
+	c.Next()
+
+	latencyDuration := time.Now().Sub(start) // アクセスからのレスポンスまでの時間
+
+	// ログ出力
+	var logger *zerolog.Event
+	baseLogger := zerolog.New(os.Stdout).
+		With().
+		Timestamp().
+		Logger()
+	if len(c.Errors) > 0 {
+		logger = baseLogger.Error().Err(c.Errors.Last())
+	} else {
+		logger = baseLogger.Info()
+	}
+	logger.
+		Str("query", c.Request.URL.RawQuery).
+		Int("status", c.Writer.Status()).
+		Str("method", c.Request.Method).
+		Str("path", c.Request.URL.RawPath).
+		Dur("latency", latencyDuration).
+		Send()
 }
 
 func Start() {
 	r := gin.Default()
+
+	// Logging middleware
 
 	// CORS middleware
 	r.Use(cors.Default())
--- backend/go.mod
+++ backend/go.mod
@@ -5,7 +5,5 @@ go 1.16
 require (
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.2
-	github.com/go-pp/pp v2.1.0+incompatible
-	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
-	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/rs/zerolog v1.25.0
 )
